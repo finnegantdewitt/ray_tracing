a85349c2fe9002967189024a155e9d38a63865d1
finnegantdewitt <finn@finnegandewitt.com>
2021-01-24 17:41:01 -0800

    rendering surface normals of sphere

--- src/main.rs
+++ src/main.rs
@@ -104,22 +104,28 @@ fn write_color(file: &mut File, display: &std::path::Display, pixel_color: Color
     }
 }
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - *center;
+    let a = r.direction().length_squared();
+    let half_b = dot(oc, r.direction());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) / (a)
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::from(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::from(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::from(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::from(0.0, 0.0, -1.0));
+        return 0.5 * Color::from(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::from(1.0, 1.0, 1.0) + t * Color::from(0.5, 0.7, 1.0)
-}
-
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
-    let oc = r.origin() - *center;
-    let a = dot(r.direction(), r.direction());
-    let b = 2.0 * dot(oc, r.direction());
-    let c = dot(oc, oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
 }
 
 fn main() {
@@ -152,7 +158,7 @@ fn main() {
         Ok(_) => {}
     }
     for j in (0..image_height).rev() {
-        println!("Scanlines remaining: {}", j);
+        //println!("Scanlines remaining: {}", j);
         for i in 0..image_width {
             let u = i as f64 / (image_width - 1) as f64;
             let v = j as f64 / (image_height - 1) as f64;
